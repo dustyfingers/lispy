ba4b777ff3d62fd7553feb8146b8e4131a639053
lou <dustyfingers01257@gmail.com>
2023-06-14 11:38:33 -0600

    make prompt platform independent

--- src/reptile.c
+++ src/reptile.c
@@ -1,27 +1,59 @@
 // reptile is the repl in the lispy ecosystem
 
 #include <stdio.h>
+#include <stdlib.h>
+
+// if we are compiling on Windows compile these functions
+#ifdef _WIN32
+#include <string.h>
 
 // declare a buffer that holds user input with a length of 2048
-static char input[2048];
+static char input_buffer[2048];
+
+// fake readline function
+char *readline(char *prompt)
+{
+    fputs(prompt, stdout);
+    fgets(input_buffer, 2048, stdin);
+    char *cpy = malloc(strlen(input_buffer) + 1);
+    strcpy(cpy, input_buffer);
+    cpy[strlen(cpy) - 1] = '\0'; // null terminator for end of string' return cpy;
+    return cpy;
+}
+
+// fake add_history function
+void add_history(char *unused) {}
+
+// otherwise include the editline headers
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+// print version and exit information
+void lispy_greeting()
+{
+
+    puts("Lispy Version 0.1");
+    puts("Press Ctrl+c to Exit\n");
+}
 
 int main(int argc, char **argv)
 {
-    // print version and exit information
-    puts("Lispy Version 0.1");
-    puts("Press Ctrl+c to Exit\n");
+    lispy_greeting();
 
     // in a never ending loop...
     while (1)
     {
-        // output out prompt
-        fputs("lispy>", stdout);
 
-        // read a line of user input of max size 2048
-        fgets(input, 2048, stdin);
+        // no matter the compilation platform, readline() will be defined!
+        char *input = readline("lispy>");
+
+        add_history(input);
 
         // echo input back to user (for now....)
-        printf("No, you %s", input);
+        printf("No, you %s\n", input);
+        free(input);
     }
 
     return 0;
